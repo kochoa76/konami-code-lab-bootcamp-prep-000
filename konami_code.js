25275cd0d616532acf01daf8be21343d7904477e
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-18 19:43:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,8 +4,10 @@ function init() {
   // Write your JavaScript code inside the init() function
 let index=0; 
 
-
-
+document.body.addEventListener('keydown', function(e)) {
+  
+  
+}
 function 
 
 for (i=0, i< code.length; i++) {
