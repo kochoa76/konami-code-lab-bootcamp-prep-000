0683e06e547ac84b958ae5c05e7b582cdc8b936c
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-18 19:50:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ document.body.addEventListener('keydown', function(e)) {
   const key = parseInt(e.detail || e.which);
   
   if (key === code[index]) {
-    index++ 
+    index++; 
     alert('Congrats!');
   }
       else {
